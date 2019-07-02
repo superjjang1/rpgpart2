6fe0d5bab7db2678b6f1e67c5e7bbf15fde2cd54
superjjang1 <51767689+superjjang1@users.noreply.github.com>
2019-07-02 09:35:23 -0400

    added zombie class to character.py

--- character.py
+++ character.py
@@ -13,3 +13,10 @@ class Character(): # character class that holds all the information for the actu
 
     def alive(self): #determines if character is alive or dead.
         return self.health > 0
+
+class Zombie(Character): #unkillable unit
+    def __init__(self):
+        super().__init__("Zombie", 0, 0) #no power, no health
+
+    def alive(self):
+        return 999999 #always alive
